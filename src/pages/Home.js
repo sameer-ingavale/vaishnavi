010d466f6692547cf0b268facf201a2c8bb94a1a
Sameer Ingavale <Sameer@Sameers-MacBook-Pro.local>
2022-10-16 17:06:44 +0530

    favicon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,20 +15,30 @@ function Home(props) {
 			<Card raised centered>
 				<Image src={vaish} wrapped ui={false} />
 				<Card.Content>
-					<Card.Header>Vaishnavi Nikam</Card.Header>
-					<Card.Meta style={{ marginTop: 4 }}>
+					<Card.Header>
+						Vaishnavi Nikam, 23
+						<Icon
+							color="blue"
+							name="check circle"
+							size="small"
+							style={{ marginLeft: 4 }}
+						/>
+					</Card.Header>
+					<Card.Meta style={{ marginTop: 5 }}>
 						<span>Taken Since September 10, 2022</span>
 					</Card.Meta>
-					<Card.Description>Back off, she's mine, bitch.</Card.Description>
+					<Card.Description style={{ paddingTop: 2 }}>
+						वैष्णवी जैसी कोई नाही
+					</Card.Description>
 					<Card.Description style={{ paddingTop: 4 }}>
 						~ Sameer Ingavale
 					</Card.Description>
 				</Card.Content>
-				<Card.Content extra>
-					<a>
+				{/* <Card.Content extra style={{ backgroundColor: '#FFF6D8' }}>
+					<div style={{ padding: 4, backgroundColor: '#FFCB38', width: '10%' }}>
 						<Icon name="user" />1 Babu
-					</a>
-				</Card.Content>
+					</div>
+				</Card.Content> */}
 			</Card>
 		</div>
 	);
