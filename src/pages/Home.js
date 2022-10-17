d25701d12dc10fb48b9f3b984400b4b702d3e5e1
Sameer Ingavale <Sameer@Sameers-MacBook-Pro.local>
2022-10-17 12:40:22 +0530

    gif

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { Image, Header, Card, Icon } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Image, Button, Card, Icon, Header } from 'semantic-ui-react';
 import vaish from '../images/vaishna.jpg';
 
 function Home(props) {
+	const [gif, setGif] = useState('');
+
+	const onClick = (event, { name }) => {
+		setGif(name);
+	};
 	return (
 		<div
 			style={{
-				height: '100vh',
+				height: '100%',
 				backgroundColor: '#FFCB38',
 				width: '100vw',
 				paddingTop: '5%',
+				paddingBottom: '5%',
 			}}
 		>
 			<Card raised centered>
@@ -25,7 +31,10 @@ function Home(props) {
 						/>
 					</Card.Header>
 					<Card.Meta style={{ marginTop: 5 }}>
-						<span>Taken Since September 10, 2022</span>
+						<span>
+							<Icon name="lock" size="small" style={{ paddingBottom: 0 }} />{' '}
+							Taken Since September 10, 2022
+						</span>
 					</Card.Meta>
 					<Card.Description style={{ paddingTop: 2 }}>
 						वैष्णवी जैसी कोई नाही
@@ -34,11 +43,52 @@ function Home(props) {
 						~ Sameer Ingavale
 					</Card.Description>
 				</Card.Content>
-				{/* <Card.Content extra style={{ backgroundColor: '#FFF6D8' }}>
-					<div style={{ padding: 4, backgroundColor: '#FFCB38', width: '10%' }}>
-						<Icon name="user" />1 Babu
+				<Card.Content extra style={{ backgroundColor: '#FFF6D8' }}>
+					<div>
+						Made with{' '}
+						<Icon
+							name="heart"
+							color="red"
+							size="small"
+							style={{ marginLeft: 2 }}
+						/>
 					</div>
-				</Card.Content> */}
+				</Card.Content>
+			</Card>
+			<Card raised centered style={{ paddingTop: 12, paddingBottom: 16 }}>
+				<div style={{ color: '#666666', marginBottom: 8 }}>
+					Does Vaish love Sameer?{'  '} 🤔
+				</div>
+
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						marginTop: 4,
+					}}
+				>
+					<Button
+						name="https://media.tenor.com/EPB7uiDGlDwAAAAC/kbc-memes-7crore-meme.gif"
+						content="Yes"
+						color="green"
+						onClick={onClick}
+					/>
+
+					<Button
+						name="https://c.tenor.com/Bplyyy9lO7EAAAAC/chal-jhootha.gif"
+						content="No"
+						color="red"
+						style={{ marginLeft: 5 }}
+						onClick={onClick}
+					/>
+				</div>
+				<Image
+					src={gif}
+					wrapped
+					ui={false}
+					style={{ marginTop: 12, paddingRight: 8, paddingLeft: 8 }}
+				/>
 			</Card>
 		</div>
 	);
